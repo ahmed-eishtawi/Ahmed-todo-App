f0dba449516a773619f8165da1975a9f80d142eb
ahmed-eishtawi <144367916+ahmed-eishtawi@users.noreply.github.com>
2024-03-22 17:50:57 +0200

    third Deploy Done (Adding 404 page)

--- src/views/PageNotFound404.vue
+++ src/views/PageNotFound404.vue
@@ -1,15 +1,13 @@
 <template>
-  <v-row class="py-3 align-center justify-center">
-    <v-col cols="12" lg="6">
-      <v-card class="w-100">
-        <img src="../img/PageNotFound.svg" alt="Error 404 / Page Not Found" />
+  <v-row class="py-0 align-center justify-center">
+    <v-col cols="12" lg="12" class="pa-0">
+      <v-card variant="" class="">
+        <img src="PageNotFound.svg" alt="Error 404 / Page Not Found" />
       </v-card>
     </v-col>
   </v-row>
 </template>
 
-<script>
-export default {};
-</script>
+<script setup></script>
 
 <style></style>
